8976a66963f6e13f784f51349be0ec3a60013ba8
jpeumesmo <joaopmoferreira@gmail.com>
2017-12-15 11:49:35 -0200

    adicao da funcionalidade de marker e htmlinfowindow

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,4 +1,4 @@
-import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions} from '@ionic-native/google-maps';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, GoogleMapOptions, MarkerOptions, Marker, HtmlInfoWindow } from '@ionic-native/google-maps';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -36,20 +36,41 @@ loadMap() {
     .then(() => {
       console.log('Map is ready!');
 
+      var contentString = '<div id="content">'+
+          '<div id="siteNotice">'+
+          '</div>'+
+          '<h1 id="firstHeading" class="firstHeading">Perdido</h1>'+
+          '<div id="bodyContent">'+
+          '<p><b>Tipo</b> Cachorro<br><b>Data</b> 15/12/2017 </p>' +
+          '<img src=file:///android_asset/www/assets/imgs/elder.jpg height="160" width="120"/>'+
+          '</div>'+
+          '</div>';
+
+      var infowindow = new HtmlInfoWindow();
+      //var infowindow = this.map.HtmlInfoWindow({
+        infowindow.setContent(contentString,{width: "210px", height: "280px"} );
+      //});
+
       // Now you can use all methods safely.
       this.map.addMarker({
-          title: 'Ionic',
+          title: 'Cachorro',
           icon: 'blue',
-          animation: 'DROP',
+//          icon: 'file:///android_asset/www/assets/imgs/elder.jpg',
+          visible: true,
+          animation: 'BOUNCE',
           position: {
-            lat: 43.0741904,
-            lng: -89.3809802
+            lat: -21.1171519,
+            lng: -44.2473204
           }
         })
         .then(marker => {
+
+          //marker.showInfoWindow();
+          //marker.setVisible(true);
+          //alert(marker.isVisible());
           marker.on(GoogleMapsEvent.MARKER_CLICK)
             .subscribe(() => {
-              alert('clicked');
+              infowindow.open(marker);
             });
         });
 
